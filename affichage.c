3f2c3d96e9fc3b434b657336991770e4ac489a84
El Haddad Louzari Moad <melhadda@localhost>
2022-03-15 15:00:27 +0100

    affichage corrigé

--- affichage.c
+++ affichage.c
@@ -6,14 +6,14 @@
 void construire_message (char *message, char motif, int lg)
 {
 int i;
-for (i=5 ; i<lg+5; i++) message[i] = motif;
+for (i=5 ; i<lg; i++) message[i] = motif;
 }
 
 void afficher_message (char *message, int lg)
 {
 int i;
 printf("message construit : ");
- for (i=0 ; i<lg+5 ; i++) printf("%c", message[i]) ;printf("]"); printf("\n");
+ for (i=0 ; i<lg ; i++) printf("%c", message[i]) ;printf("]"); printf("\n");
 }
 
 
